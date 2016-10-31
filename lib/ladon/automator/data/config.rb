a9302dd93ebcaf6d207e008cf112cf257fa5a71e
Shayne Snow <ssnow@athenahealth.com>
2016-10-31 08:29:54 -0400

    Add context support to Automator configs

--- lib/ladon/automator/data/config.rb
+++ lib/ladon/automator/data/config.rb
@@ -5,12 +5,13 @@ module Ladon
     class Config
       include Ladon::HasContexts
 
-      attr_accessor :id, :log_level
+      attr_accessor :id, :log_level, :contexts
 
       # Create a new Automator Config instance.
-      def initialize(id: SecureRandom.uuid, log_level: :ERROR)
+      def initialize(id: SecureRandom.uuid, log_level: :ERROR, contexts: {})
         @id = id
         @log_level = log_level
+        self.contexts = contexts
       end
     end
   end
