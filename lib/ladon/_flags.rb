20ec49e0e2c01c255be68c844fd9f183225c3042
Shayne Snow <ssnow@athenahealth.com>
2016-11-07 10:57:46 -0500

    Fixes to flags and ladon-run

--- lib/ladon/_flags.rb
+++ lib/ladon/_flags.rb
@@ -6,8 +6,7 @@ module Ladon
     # @param [Hash] in_hash Optional hash used to fill out the new +Flags+ instance.
     def initialize(in_hash: nil)
       @flags = {}
-
-      in_hash.each { |key, val| @flags[key] = val } if in_hash.is_a?(Hash)
+      in_hash.each { |key, val| @flags[key.to_sym] = val } if in_hash.is_a?(Hash)
 
       # once created, flags should not be modified
       @flags.freeze
@@ -20,7 +19,7 @@ module Ladon
     # @param [Object] default_to Defaut value to assume if the given +flag+ isn't found.
     # @return [Object] The object registered with the given +flag+ name if it exists, else +default_to+.
     def get(flag, default_to:)
-      @flags.fetch(flag, default_to)
+      @flags.fetch(flag.to_sym, default_to)
     end
   end
 end
