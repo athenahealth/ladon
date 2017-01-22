1f9e65830938090a1787fdebfb41c5f322f851a1
Kevin Weaver <kweaver@athenahealth.com>
2017-01-22 15:36:33 -0500

    Added State.transition method and tests.

--- spec/modeler/components/state_spec.rb
+++ spec/modeler/components/state_spec.rb
@@ -1,13 +1,52 @@
 require 'spec_helper'
 require 'ladon'
 
+class ExampleState < Ladon::Modeler::State
+  transition 'ExampleTransition1' do |t|
+    t.meta('index', 1)
+  end
+
+  transition 'ExampleTransition1' do |t|
+    t.meta('index', 1)
+  end
+end
+
+class ExampleStateSubclass < ExampleState
+  transition 'ExampleTransition2' do |t|
+    t.meta('index', 3)
+  end
+
+  transition 'ExampleTransition3' do |t|
+    t.meta('index', 4)
+  end
+end
+
 module Ladon
   module Modeler
     RSpec.describe State do
       describe '.transitions' do
-        subject { -> { State.transitions } }
+        context 'when .transitions is not overridden' do
+          subject { State.transitions }
 
-        it { is_expected.to raise_error(MissingImplementationError, 'self.transitions') }
+          it { is_expected.to be_an(Enumerable) }
+          it { is_expected.to be_empty }
+        end
+
+        context 'when .transition is used' do
+          subject { ExampleState.transitions }
+
+          it { is_expected.to be_an(Enumerable) }
+          it { is_expected.to satisfy { |ts| ts.length == 2 } }
+          it { is_expected.to include(a_kind_of(Ladon::Modeler::Transition)) }
+        end
+
+        context 'when .transition is used in subclass and parent' do
+          subject { ExampleStateSubclass.transitions }
+
+          it { is_expected.to be_an(Enumerable) }
+          it { is_expected.to satisfy { |ts| ts.length == 4 } }
+          it { is_expected.to include(a_kind_of(Ladon::Modeler::Transition)) }
+        end
       end
     end
   end
