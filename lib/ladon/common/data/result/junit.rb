9e7ff575a59c5ac14d846eefbba1ee2aadd65e36
Reagan Eggert <reggert@athenahealth.com>
2017-03-25 18:58:46 -0400

    Pull path from config instead of class_name

--- lib/ladon/common/data/result/junit.rb
+++ lib/ladon/common/data/result/junit.rb
@@ -3,15 +3,27 @@ require 'nokogiri'
 module Ladon
   # Generates JUnit XML for Jenkins.
   class JUnit
+    # Generate XML according to the JUnit schema to Jenkins consumption.
+    #
+    # @param [String] status The status of the automation.
+    # @param [Ladon::Config] config The config used to instantiate the Ladon
+    #   object.
+    # @param [Float] time The duration of the automation runtime in minutes.
+    # @param [String] log The stringified Ladon::Logging::Logger.
+    #
+    # @return [String] The stringified XML.
     def self.generate(status:, config:, time:, log:)
+      time *= 60 # JUnit expects in seconds but Ladon records in minutes
+      job_name = convert_target_path_to_job_name(config.flags.target_path)
+
       builder = Nokogiri::XML::Builder.new do |xml|
         xml.testsuite(
-          name: config.class_name,
-          time: time * 60, # JUnit expects in seconds but Ladon records in minutes
+          name: job_name,
+          time: time,
           tests: 1 # Only one test per result
         ) do
           xml.testcase(
-            classname: config.class_name,
+            classname: job_name,
             time: time
           ) do
             xml.send(status.downcase.to_sym, status) if status != 'SUCCESS'
@@ -24,4 +36,8 @@ module Ladon
       builder.to_xml
     end
   end
+end
+
+def convert_target_path_to_job_name(target_path)
+  target_path.match(%r{automations/(.+)\.rb})[1].tr!('/', '.')
 end
