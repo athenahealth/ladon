51a546bfa0d92bf75f0c5ce7a257035b05793b3b
Astha Patni [apatni] <apatni@athenahealth.com>
2017-08-01 11:50:02 -0400

    Rounding is weird with the timer spec

--- spec/common/timing/timer_spec.rb
+++ spec/common/timing/timer_spec.rb
@@ -58,7 +58,7 @@ module Ladon
             # sleep in seconds
             subject.for(timer_name) { sleep 0.5 }
             # duration should be identical, but in minutes
-            expect(subject.total_time.round(4)).to eq((0.5 / 60).round(4))
+            expect(subject.total_time.round(3)).to eq((0.5 / 60).round(3))
           end
         end
 
@@ -68,7 +68,7 @@ module Ladon
             subject.for(timer_name) { sleep 0.25 }
             subject.for(timer_name) { sleep 0.5 }
             # duration should be identical to sum, but in minutes
-            expect(subject.total_time.round(4)).to eq(((0.25 + 0.5) / 60).round(4))
+            expect(subject.total_time.round(3)).to eq(((0.25 + 0.5) / 60).round(3))
           end
         end
       end
