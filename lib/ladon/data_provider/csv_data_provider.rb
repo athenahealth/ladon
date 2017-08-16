a159571280d4b8d320189877bd827af327ff9bc6
Sethu Rajan Muthiah [msethurajan] <msethurajan@athenahealth.com>
2017-08-16 16:37:53 +0530

    Data provider changes

--- lib/ladon/data_provider/csv_data_provider.rb
+++ lib/ladon/data_provider/csv_data_provider.rb
@@ -15,39 +15,39 @@ class CSVDataProvider
   # @parame input_file_name [String] The path of the csv file.
   # @param row_num [Array<Integer>] The indexes of the row.
   #
-  # @return excel_data [Array of hashes] The data needed for the script.
+  # @return csv_data [Array of hashes] The data needed for the script.
   def get_data(
     input_file_name:,
     row_num: nil
   )
-    excel_raw_data = get_file_input_stream(input_file_name)
-    header = excel_raw_data[0]
+    csv_raw_data = get_file_input_stream(input_file_name)
+    header = csv_raw_data[0]
     header.map!(&:to_sym)
-    row_num = (1..excel_raw_data.length - 1).to_a if row_num[0].eql? 'all'
-    hashify_rows(row_num: row_num, excel_raw_data: excel_raw_data, header: header)
+    row_num = (1..csv_raw_data.length - 1).to_a if row_num[0].eql? 'all'
+    _hashify_rows(row_num: row_num, raw_data: csv_raw_data, header: header)
   end
 
   # Forms the Array of hashes with the data in the CSV.
   #
   # @param row_num [Array<Integer>] The indexes of the row.
-  # @param excel_raw_data [Array<Hash>] The data in the CSV.
+  # @param raw_data [Array<Hash>] The data in the CSV.
   # @param header [Array] The first header row of the CSV data.
   #
-  # @return excel_data [Array<Hash>] The array of hashes of rows of CSV.
-  def hashify_rows(
+  # @return data [Array<Hash>] The array of hashes of rows of CSV.
+  def _hashify_rows(
     row_num:,
-    excel_raw_data:,
+    raw_data:,
     header:
   )
-    excel_data = []
+    data = []
     row_num.each do |i|
-      row = Hash[header.zip(excel_raw_data[i])]
+      row = Hash[header.zip(raw_data[i])]
       row.each_key do |r|
         row[r] = nil if row[r].eql? 'nil'
         row[r] = row[r].to_s.strip if row[r]
       end
-      excel_data << row
+      data << row
     end
-    excel_data
+    data
   end
 end
